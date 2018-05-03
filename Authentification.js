4d95f3e2ac67f04f96ae48e8a084cae70fae6d60
Marilyth <baermay98@gmail.com>
2018-05-03 13:02:56 +0200

    try show discord response

--- Authentification.js
+++ Authentification.js
@@ -57,7 +57,16 @@ function getUser(){
     var request = new XMLHttpRequest();
 
     request.onreadystatechange = function () {
-        showDict(JSON.parse(request.responseText));
+        document.body.innerHTML = `<table border="1" id="parameters">
+        </table>
+        <script src="./Authentification.js"></script>
+        <script>
+            getToken();
+        </script>
+        <button type="button" onclick="refreshToken()"> Refresh </button>
+        <button type="button" onclick="getUser()"> Fetch User Information </button>
+        <button type="button" onclick="getGuilds()"> Fetch Users Guilds </button>
+        ${JSON.parse(request.responseText)}`;
     }
 
     request.open("GET", `${APIENDPOINT}/users/@me`, false);
@@ -73,8 +82,16 @@ function getGuilds(){
         var dict = (JSON.parse(request.responseText).map(function(x){
             return {GuildName: x["name"]}
         }));
-        console.log(dict);
-        showDict(dict);
+        document.body.innerHTML = `<table border="1" id="parameters">
+        </table>
+        <script src="./Authentification.js"></script>
+        <script>
+            getToken();
+        </script>
+        <button type="button" onclick="refreshToken()"> Refresh </button>
+        <button type="button" onclick="getUser()"> Fetch User Information </button>
+        <button type="button" onclick="getGuilds()"> Fetch Users Guilds </button>
+        ${dict}`;
     }
 
     request.open("GET", `${APIENDPOINT}/users/@me/guilds`, false);
