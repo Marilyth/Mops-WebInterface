0bc2488fb7e8a3610e6f6f29732bf00b29357295
Marilyth <baermay98@gmail.com>
2018-05-04 13:59:05 +0200

    typo

--- UserInterface.js
+++ UserInterface.js
@@ -78,9 +78,8 @@ function displayOptions(){
 
     display.appendChild(table);
 
-    var toPopOut = document.querySelectorAll('.zoomBox');
+    var toPopIn = document.querySelectorAll('.zoomBox');
     window.setTimeout(Array.prototype.forEach.call(toPopIn, x => {
-        console.log('I was called');
-        x.style = "width: 64px; height: 64px; transition: all 0.3s ease; opacity: 1;";
+        x.style = "width: 64px; height: 64px; transition: width, height, opacity 0.3s ease; opacity: 1;";
     }), 300);
 }
