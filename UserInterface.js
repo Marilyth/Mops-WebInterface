b8c0937367fa67d6403030da42ab36b93a4b1814
Marilyth <baermay98@gmail.com>
2018-05-04 11:40:02 +0200

    now thats fucking weird

--- UserInterface.js
+++ UserInterface.js
@@ -31,7 +31,7 @@ function displayGuilds() {
             count = 0;
         }
     });
-    document.getElementById("iconList").innerHTML = table.innerHTML;
+    document.getElementById("iconList").appendChild(table);
 }
 
 function switchToGuild(guild) {
@@ -41,7 +41,7 @@ function switchToGuild(guild) {
     });
     var guildDisplay = document.createElement('table');
     var row = guildDisplay.insertRow(-1);
-    row.insertCell(-1).innerHTML = `<img class="roundSquare" style="width: 0px; height: 0px; transition: all 0.3s ease; width: 128px; height: 128px;" src="https://cdn.discordapp.com/icons/${guild["id"]}/${guild["icon"]}.png">`;
+    row.insertCell(-1).innerHTML = `<img class="roundSquare" style="width: 0%; height: 0%; transition: all 0.3s ease; width: 100%; height: 100%" src="https://cdn.discordapp.com/icons/${guild["id"]}/${guild["icon"]}.png">`;
     row.insertCell(-1).innerHTML = `<p>Name: ${guild["name"]}</p><p>ID: ${guild["id"]}</p>`;
     document.getElementById('topIcon').innerHTML = guildDisplay.innerHTML;
 }
