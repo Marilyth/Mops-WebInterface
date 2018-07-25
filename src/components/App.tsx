6830e837e0e7b42e7a1da802d2cf915ebb12b2d2
Sebastian Kramm <sebastian.kramm@protonmail.com>
2018-07-25 16:52:28 +0200

    guild scroll list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router';
 
 // import {isLoggedIn} from './../utils/Authentification'
+import Guilds from './Guilds'
 import Login from './Login';
 // import AuthButton from './AuthButton';
 import TokenReader from './TokenReader';
@@ -24,18 +25,37 @@ function about() {
     return <div>About</div>;
 }
 
-class App extends React.Component{
+function serverList(){
+    return <div className = 'centralContainer'>
+                <table id='containerTable'>
+                    <tr>
+                        <td>
+                            <div id='serverList' className='scroll-content serverList'>
+                                <Guilds/>
+                            </div>
+                        </td>
+                        <td>
+                            <div id="iconList" className='iconList'> bla </div>
+                        </td>
+                    </tr>
+                    </table>
+           </div>
+}
+
+class App extends React.Component {
     public render() {
         return (
             <div>
                 <div className='topContainer'>
-                    <Login/>
+                    <Login />
                 </div>
+                
 
                 <Switch>
                     <Route exact={true} path='/' render={test} />
                     <Route path='/Redirect' component={TokenReader} />
                     <Route path='/About' component={about} />
+                    <Route patch='/Guilds' component={serverList} />
                     <Route component={NoMatch} />
                 </Switch>
             </div>
--- src/components/Guilds.tsx
+++ src/components/Guilds.tsx
@@ -1,7 +1,58 @@
-// import * as React from 'react';
+import * as React from 'react';
 
-// class Guilds extends React.Component{
+class Guilds extends React.Component {
 
-// }
+    private lastGuild: any;
 
-// export default Guilds
+
+
+    public Guilds() {
+        this.lastGuild = null;
+    }
+
+    public render() {
+        const guilds = JSON.parse(sessionStorage.getItem('guilds') || "");
+
+
+        return <table id="guilds" className='guildTable'>
+            {guilds.map((guild: any) => this.tabelRow(guild))}
+        </table>
+    }
+
+    public tabelRow = (guild: any) => {
+        return <tr>
+            <td id={guild.id}>
+                <img title={guild.name} onClick={// tslint:disable-next-line:jsx-no-lambda no-shadowed-variable
+                    () => { this.switchToGuild(guild) }} className='roundSquare guildIcon' src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`}
+                />
+            </td>
+        </tr>
+    }
+
+
+    public switchToGuild = (guild: any) => {
+        if (this.lastGuild != null) {
+            const toReverse = document.getElementById(`image:${this.lastGuild.id}`);
+            if (toReverse) {
+                toReverse.className = "roundSquare toReverseElement";
+            }
+        }
+        const chosen = document.getElementById(`image:${guild.id}`);
+        if (chosen) {
+            chosen.className = "selected-image chosenElement";
+        }
+        this.lastGuild = guild;
+        this.displayOptions(guild);
+    }
+
+    private displayOptions(guild: any) {
+        // tslint:disable-next-line:no-console
+        console.log(guild.id)
+    }
+
+
+
+}
+
+
+export default Guilds
