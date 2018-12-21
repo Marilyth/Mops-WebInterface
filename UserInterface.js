f88546772c475355c1f88678ca3e724755147a0f
May Baer <baermay98@gmail.com>
2018-12-21 16:16:04 +0100

    getValues properly implemented

--- UserInterface.js
+++ UserInterface.js
@@ -20,7 +20,7 @@ function displayGuilds() {
         image.onclick = function () { switchToGuild(guild) };
         image.style = "width: 46px; heigth: 46px; margin: 0 auto; display:block;";
         image.title = guild["name"];
-        if(`${guild['icon']}` != 'null')
+        if (`${guild['icon']}` != 'null')
             image.src = `https://cdn.discordapp.com/icons/${guild["id"]}/${guild["icon"]}.png`;
         else
             image.src = `https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Icon-round-Question_mark.svg/600px-Icon-round-Question_mark.svg.png`;
@@ -36,7 +36,7 @@ function displayGuilds() {
 }
 
 function switchToGuild(guild) {
-    if(lastGuild !== null){
+    if (lastGuild !== null) {
         var toReverse = document.getElementById(`image:${lastGuild['id']}`);
         toReverse.className = "roundSquare";
         toReverse.style = "width: 46px; height: 46px; margin: 0 auto; display:block; border-radius: 50%; transform: scale(1, 1);";
@@ -49,7 +49,7 @@ function switchToGuild(guild) {
     displayOptions(guild);
 }
 
-function displayOptions(){
+function displayOptions() {
     var display = document.getElementById('optionList');
     display.innerHTML = "";
 
@@ -62,34 +62,36 @@ function displayOptions(){
 
     var options = getOptions();
 
-    for(var category in options){
+    for (var category in options) {
         var acHeader = document.createElement('div');
         acHeader.classList = 'accordion';
         acHeader.id = category + 'AccordionHeader';
         acHeader.innerText = category;
 
-        for(var index in options[category]){
+        for (var index in options[category]) {
             var commandButton = document.createElement('div');
             commandButton.className = 'channelButton';
             commandButton.innerText = '# ' + options[category][index];
             var commandName = options[category][index];
             commandButton.style.paddingLeft = '5px';
-            (function(commandName){commandButton.addEventListener('click', (event) => {
-                displayContent(commandName);
-                event.stopPropagation();
-            });})(commandName);
+            (function (commandName) {
+                commandButton.addEventListener('click', (event) => {
+                    displayContent(commandName);
+                    event.stopPropagation();
+                });
+            })(commandName);
             acHeader.appendChild(commandButton);
         }
 
         table.insertRow(-1).insertCell(-1).appendChild(acHeader);
-        acHeader.onclick = (function(acHeader){return function(){expandAccordion(acHeader);}})(acHeader);
+        acHeader.onclick = (function (acHeader) { return function () { expandAccordion(acHeader); } })(acHeader);
         console.log(acHeader);
     }
 
     display.appendChild(table);
 }
 
-function displayContent(option){
+function displayContent(option) {
     var display = document.getElementById('contentList');
     display.innerHTML = "";
 
@@ -102,142 +104,31 @@ function displayContent(option){
     firstRow.innerHTML = `<div id='optionName' style='text-align: center !important; width: 100%;'>${option}</div>`;
 
     getContent(option);
-
-    for(var curContent in content['Content']){
-        var acHeader = document.createElement('div');
-        acHeader.classList = 'accordion';
-        acHeader.style = 'text-align: center !important; background: rgb(72,75,81) !important;';
-        acHeader.id = content['Content'][curContent]['Name'] + 'AccordionHeader';
-        acHeader.innerText = content['Content'][curContent]['Name'];
-
-        var innerTable = document.createElement('table');
-        innerTable.style = "border-collapse: collapse; border-spacing: 3px 3px; width: 100%; text-align: left;";
-
-        for(var parameter in content['Content'][curContent]){
-            var input = document.createElement('input');
-            var description = document.createElement('div');
-            var row = innerTable.insertRow(-1);
-            var cellA = row.insertCell(-1);
-            var cellB = row.insertCell(-1);
-
-            description.innerText = parameter + ': ';
-            description.style.fontSize = '15px';
-            input.placeholder = content['Content'][curContent][parameter];
-            input.required = parameter == 'Name' || parameter == 'Channel' ? true : false;
-            input.readOnly = parameter == 'Name' ? true : false;
-            input.value = parameter == 'Name' ? content['Content'][curContent][parameter] : '';
-            input.id = content['Content'][curContent]['Name'] + parameter;
-            input.style.color = 'rgb(246, 246, 247)';
-            input.style.background = 'rgb(72, 75, 81)';
-            input.style.border = '0';
-            input.style.borderBottom = '1px solid rgb(54, 57, 63)';
-            input.style.outline = 'none';
-            input.addEventListener('click', (event) => {event.stopPropagation();});
-            cellA.style.width = '20%';
-            cellA.appendChild(description);
-            cellB.style.width = '80%';
-            cellB.appendChild(input);
-            cellB.addEventListener('click', (event) => {event.stopPropagation();});
-        }
-
-        var update = document.createElement('button');
-        update.className = 'invite-button';
-        update.innerText = 'Update';
-        update.style.color = 'rgb(67, 181, 129)';
-        update.style.background = 'rgb(72,75,81)';
-        update.style.border = '2px solid rgb(67, 181, 129)';
-        update.style.width = '100%';
-        update.onclick = function(event){updateContent(option, content['Content'][curContent]['Name'], getValues(content['Parameters'], content['Content'][curContent]['Name'])); event.stopPropagation();};
-        var remove = document.createElement('button');
-        remove.className = 'invite-button';
-        remove.style.color = 'rgb(212, 56, 56)';
-        remove.style.background = 'rgb(72,75,81)';
-        remove.style.border = '2px solid rgb(212, 56, 56)';
-        remove.style.width = '25%';
-        remove.innerText = 'Remove';
-
-        var row = innerTable.insertRow(-1);
-        var cellA = row.insertCell(-1);
-        var cellB = row.insertCell(-1);
-        cellB.style.textAlign = 'right';
-        cellB.style.paddingTop = '20px';
-        cellA.style.paddingTop = '20px';
-        cellA.appendChild(update);
-        cellB.appendChild(remove);
-
-        acHeader.appendChild(innerTable);
-
-        table.insertRow(-1).insertCell(-1).appendChild(acHeader);
-        acHeader.onclick = (function(acHeader){return function(){expandAccordion(acHeader, true);}})(acHeader);
-        console.log(acHeader);
+    var headers = makeInputTable(option);
+    console.log(headers);
+    for (var index in headers) {
+        var curHeader = headers[index];
+        table.insertRow(-1).insertCell(-1).appendChild(headers[index]);
+        headers[index].onclick = (function (curHeader) { return function () { expandAccordion(curHeader); } })(curHeader);
+        console.log(headers[index]);
     }
 
-    //Add option for adding new element
-    var acHeader = document.createElement('div');
-        acHeader.classList = 'accordion';
-        acHeader.style = 'text-align: center !important; background: rgb(72,75,81) !important;';
-        acHeader.id = 'NewAccordionHeader';
-        acHeader.innerText = '+New+';
-
-        var innerTable = document.createElement('table');
-        innerTable.style = "border-collapse: collapse; border-spacing: 3px 3px; width: 100%; text-align: left;";
-
-        for(var parameter in content['Parameters']){
-            var input = document.createElement('input');
-            var description = document.createElement('div');
-            var row = innerTable.insertRow(-1);
-            var cellA = row.insertCell(-1);
-            var cellB = row.insertCell(-1);
-
-            description.innerText = content['Parameters'][parameter] + ': ';
-            description.style.fontSize = '15px';
-            input.id = 'New' + content['Parameters'][parameter];
-            input.style.color = 'rgb(246, 246, 247)';
-            input.style.background = 'rgb(72, 75, 81)';
-            input.style.border = '0';
-            input.style.borderBottom = '1px solid rgb(54, 57, 63)';
-            input.onclick = function(event){event.stopPropagation();};
-            cellA.style.width = '20%';
-            cellA.appendChild(description);
-            cellB.style.width = '80%';
-            cellB.appendChild(input);
-        }
-
-        acHeader.appendChild(innerTable);
-        
-        var div = document.createElement('div');
-        div.style.paddingTop = '20px';
-        var add = document.createElement('button');
-        add.className = 'invite-button';
-        add.innerText = 'Add';
-        add.style.color = 'rgb(67, 181, 129)';
-        add.style.background = 'rgb(72,75,81)';
-        add.style.border = '2px solid rgb(67, 181, 129)';
-        add.style.width = '100%';
-        add.style.alignSelf = 'center';
-        add.onclick = function(event){addContent(option, 'New', getValues(content['Parameters'], 'New')); event.stopPropagation();};
-        div.appendChild(add);
-
-        acHeader.appendChild(div);
-        acHeader.onclick = (function(acHeader){return function(){expandAccordion(acHeader, true);}})(acHeader);
-
-        table.insertRow(-1).insertCell(-1).appendChild(acHeader);
-
+    table.insertRow(-1).insertCell(-1).appendChild(makeNewInput(option));
     display.appendChild(table);
 }
 
-function expandAccordion(header, keepCenter = false){
+function expandAccordion(header) {
     var count = (header.innerHTML.match(/div>/g) || []).length;
     var count2 = (header.innerHTML.match(/button>/g) || []).length;
-    var newHeight = (count + count2)*31 + 30;
+    var newHeight = (count + count2) * 31 + 30;
     header.style.height = newHeight + 'px';
-    console.log("Expanded " + header.id + " to "  + count + " Elements")
-    header.onclick = function() {compressAccordion(header, keepCenter);};
+    console.log("Expanded " + header.id + " to " + count + " Elements")
+    header.onclick = function () { compressAccordion(header); };
 }
 
-function compressAccordion(header, keepCenter = false){
+function compressAccordion(header) {
     header.style.height = '27px';
-    header.onclick = function() {expandAccordion(header, keepCenter);};
+    header.onclick = function () { expandAccordion(header); };
 }
 
 function getChannels() {
@@ -246,7 +137,7 @@ function getChannels() {
 
 function getOptions() {
     var optionsDict = {};
-    optionsDict['Trackers'] = ["Osu", "Twitch", "Twitter", "Youtube", "YoutubeLive", "Reddit", "HTML", "WoW", "OSRS", "TwitchClip", "News", "Overwatch"];
+    optionsDict['Trackers'] = ["OsuTracker", "TwitchTracker", "TwitterTracker", "YoutubeTracker", "YoutubeLiveTracker", "RedditTracker", "HTMLTracker", "WoWTracker", "OSRSTracker", "TwitchClipTracker", "NewsTracker", "OverwatchTracker"];
     //optionsDict['Information'] = ["GetStats"];
     //optionsDict['Moderation'] = ["Poll", "RoleInvite", "Giveaway"];
 
@@ -265,21 +156,21 @@ function getContent(type) {
         }
     }
     console.log(type);
-    request.open("GET", `http://5.45.104.29:5000/api/tracker?guild=${lastGuild['id']}&type=${type}`, false);
+    request.open("GET", `http://5.45.104.29:5000/api/content?guild=${lastGuild['id']}&type=${type}`, false);
     request.send();
 }
 
-function removeContent(type, name, channel){
+function removeContent(type, name, channel) {
 
 }
 
-function updateContent(type, name, contentDict){
+function updateContent(type, name, contentDict) {
 
 }
 
-function addContent(type, name, contentDict){
+function addContent(type, name, contentDict) {
     //Send stuff to Mops-API
-    if(false){
+    if (false) {
         //Display content again, this time with added content
         console.log('Added content! o:');
         displayContent();
@@ -288,12 +179,174 @@ function addContent(type, name, contentDict){
     }
 }
 
-function getValues(parameters, name){
-    dict = {};
-    for(index in parameters){
-        var curValue = document.getElementById(name + parameters[index]).value;
-        dict[parameters[index]] = curValue;
+function getValues(index, name) {
+    dict = {"NewValue": {}, "OldValue": content["Content"][index]};
+
+    for (entry in content["Parameters"]) {
+        var curValue = document.getElementById(name + entry).value;
+        dict["NewValue"][entry] = curValue;
     }
+
     console.log(dict);
     return dict;
+}
+
+function makeInputTable(option) {
+    var headers = [];
+    for (var curContent in content['Content']) {
+        var acHeader = document.createElement('div');
+        acHeader.classList = 'accordion';
+        acHeader.style = 'text-align: center !important; background: rgb(72,75,81) !important;';
+        acHeader.id = content['Content'][curContent]['Name'] + 'AccordionHeader';
+        acHeader.innerText = content['Content'][curContent]['Name'];
+
+        var innerTable = document.createElement('table');
+        innerTable.style = "border-collapse: collapse; border-spacing: 3px 3px; width: 100%; text-align: left; color: rgb(246, 246, 247);";
+
+        for (var parameter in content['Content'][curContent]) {
+            var input;
+            var description = document.createElement('div');
+            var row = innerTable.insertRow(-1);
+            var cellA = row.insertCell(-1);
+            var cellB = row.insertCell(-1);
+
+            description.innerText = parameter + ': ';
+            description.style.fontSize = '15px';
+
+            if (Array.isArray(content["Parameters"][parameter])) {
+                input = document.createElement('select');
+
+                for (var optionIndex in content["Parameters"][parameter]) {
+                    var optionValue = document.createElement('option');
+                    optionValue.text = content["Parameters"][parameter][optionIndex];
+                    optionValue.value = content["Parameters"][parameter][optionIndex];
+                    if(content["Parameters"][parameter][optionIndex] == content['Content'][curContent][parameter]){
+                        optionValue.selected = true;
+                    }
+                    
+                    input.appendChild(optionValue);
+                }
+            } else {
+                input = document.createElement('input')
+                input.value = content['Content'][curContent][parameter];
+            }
+
+            input.id = content['Content'][curContent]['Name'] + parameter;
+            input.style.color = 'rgb(246, 246, 247)';
+            input.style.background = 'rgb(72, 75, 81)';
+            input.style.border = '0';
+            input.style.width = '100%';
+            input.style.borderBottom = '1px solid rgb(54, 57, 63)';
+            input.style.outline = 'none';
+            input.addEventListener('click', (event) => { event.stopPropagation(); });
+            cellA.style.width = '20%';
+            cellA.appendChild(description);
+            cellB.style.width = '80%';
+            cellB.appendChild(input);
+            cellB.addEventListener('click', (event) => { event.stopPropagation(); });
+        }
+
+        var update = document.createElement('button');
+        update.className = 'invite-button';
+        update.innerText = 'Update';
+        update.style.color = 'rgb(67, 181, 129)';
+        update.style.background = 'rgb(72,75,81)';
+        update.style.border = '2px solid rgb(67, 181, 129)';
+        update.style.width = '100%';
+        var curName = content['Content'][curContent]['Name'];
+        console.log(curName);
+        //update.onclick = (function (curName, event) { updateContent(option, curName, getValues(content['Parameters'], name)); event.stopPropagation(); })(curName, event);
+        (function(curName, curContent){update.addEventListener('click', (event) => {updateContent(option, curName, getValues(curContent, curName)); event.stopPropagation();})})(curName, curContent);
+        var remove = document.createElement('button');
+        remove.className = 'invite-button';
+        remove.style.color = 'rgb(212, 56, 56)';
+        remove.style.background = 'rgb(72,75,81)';
+        remove.style.border = '2px solid rgb(212, 56, 56)';
+        remove.style.width = '25%';
+        remove.innerText = 'Remove';
+
+        var row = innerTable.insertRow(-1);
+        var cellA = row.insertCell(-1);
+        var cellB = row.insertCell(-1);
+        cellB.style.textAlign = 'right';
+        cellB.style.paddingTop = '20px';
+        cellA.style.paddingTop = '20px';
+        cellA.appendChild(update);
+        cellB.appendChild(remove);
+
+        acHeader.appendChild(innerTable);
+        headers.push(acHeader);
+    }
+
+    return headers;
+}
+
+function makeNewInput(option) {
+    var acHeader = document.createElement('div');
+    acHeader.classList = 'accordion';
+    acHeader.style = 'text-align: center !important; background: rgb(72,75,81) !important;';
+    acHeader.id = 'NewAccordionHeader';
+    acHeader.innerText = '+New+';
+
+    var innerTable = document.createElement('table');
+    innerTable.style = "border-collapse: collapse; border-spacing: 3px 3px; width: 100%; text-align: left; color: rgb(246, 246, 247);";
+
+    for (var parameter in content['Parameters']) {
+        var input;
+        var description = document.createElement('div');
+        var row = innerTable.insertRow(-1);
+        var cellA = row.insertCell(-1);
+        var cellB = row.insertCell(-1);
+
+        description.innerText = parameter + ': ';
+        description.style.fontSize = '15px';
+
+        if (Array.isArray(content["Parameters"][parameter])) {
+            input = document.createElement('select');
+            input.required = true;
+            for (var optionIndex in content["Parameters"][parameter]) {
+                var optionValue = document.createElement('option');
+                optionValue.text = content["Parameters"][parameter][optionIndex];
+                optionValue.value = content["Parameters"][parameter][optionIndex];
+                input.appendChild(optionValue);
+            }
+        } else {
+            input = document.createElement('input')
+        }
+
+        input.id = 'New' + parameter;
+        input.value = content['Parameters'][parameter];
+        input.style.width = '100%';
+        input.style.color = 'rgb(246, 246, 247)';
+        input.style.background = 'rgb(72, 75, 81)';
+        input.style.border = "0";
+        input.style.borderBottom = '1px solid rgb(54, 57, 63)';
+        input.style.outline = 'none';
+        input.onclick = function (event) { event.stopPropagation(); };
+
+        cellA.style.width = '20%';
+        cellA.appendChild(description);
+        cellB.style.width = '80%';
+        cellB.appendChild(input);
+    }
+
+    acHeader.appendChild(innerTable);
+
+    var div = document.createElement('div');
+    div.style.paddingTop = '20px';
+    var add = document.createElement('button');
+    add.className = 'invite-button';
+    add.innerText = 'Add';
+    add.style.color = 'rgb(67, 181, 129)';
+    add.style.background = 'rgb(72,75,81)';
+    add.style.border = '2px solid rgb(67, 181, 129)';
+    add.style.width = '100%';
+    add.style.alignSelf = 'center';
+    add.onclick = function (event) { addContent(option, 'New', getValues(content['Parameters'], 'New')); event.stopPropagation(); };
+    div.appendChild(add);
+
+    acHeader.appendChild(div);
+    acHeader.onclick = (function (acHeader) { return function () { expandAccordion(acHeader); } })(acHeader);
+
+    return acHeader;
 }
